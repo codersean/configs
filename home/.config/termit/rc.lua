b52dc557a3071f4c22b741a851c5b33c53287267
Sean Long <me@sean-long.com>
2014-06-05 00:00:42 -0700

    Added config for termit

---
+++ home/.config/termit/rc.lua
@@ -0,0 +1,19 @@
+require("termit.colormaps")
+require("termit.utils")
+
+defaults = {}
+defaults.scrollbackLines = 1000
+defaults.geometry = '110x36'
+defaults.hideSingleTab = true
+defaults.transparency = 0.5
+
+bindKey('CtrlShift-c',    copy)
+bindKey('CtrlShift-v',    paste)
+bindKey('CtrlShift-w',    closeTab)
+bindKey('CtrlShift-q',    quit)
+
+bindKey('Ctrl-F',         findDlg)
+bindKey('Ctrl-Page_Up',   prevTab)
+bindKey('Ctrl-Page_Down', nextTab)
+
+setOptions(defaults)
